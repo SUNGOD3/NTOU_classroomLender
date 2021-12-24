f379cc7067aac1336029a627b695f844164458af
SUNGOD3 <72863539+SUNGOD3@users.noreply.github.com>
2021-12-25 03:53:04 +0800

    selectClassroom api
    and fix some bug

--- backend/api/modules/Classrooms.py
+++ backend/api/modules/Classrooms.py
@@ -1,3 +1,5 @@
+from os import error
+from pymysql.cursors import Cursor
 from flask import Blueprint,request,jsonify,session
 import pymysql
 import yaml
@@ -46,7 +48,7 @@ def addClassroom():
 
 @Classrooms.route('/deleteClassroom',methods=['POST'])
 def deleteClassroom():
-    #get data in form by name from HTML
+    #get data in form by name from mySQL
     connection = pymysql.connect(host=cfg['db']['host'],user=cfg['db']['user'],password=cfg['db']['password'],db=cfg['db']['database'])
     #build dictionary
     info = dict()
@@ -63,7 +65,7 @@ def deleteClassroom():
 
 @Classrooms.route('/changeClassroom',methods=['POST'])
 def changeClassroom():
-    #get data in form by name from HTML
+    #get data in form by name from mySQL
     connection = pymysql.connect(host=cfg['db']['host'],user=cfg['db']['user'],password=cfg['db']['password'],db=cfg['db']['database'])
     #build dictionary
     info = dict()
@@ -86,4 +88,41 @@ def changeClassroom():
         traceback.print_exc()
         connection.rollback()
         info['errors'] = 'changeClassroom fail'
+    return jsonify(info)
+
+@Classrooms.route('/selectClassroom',methods=['POST'])
+def selectClassroom():
+    #get data in form by name from mySQL
+    connection = pymysql.connect(host=cfg['db']['host'],user=cfg['db']['user'],password=cfg['db']['password'],db=cfg['db']['database'])
+    #build dictionary
+    info = dict()
+    errors = []
+    cursor = connection.cursor()
+    info['classroomID'] = request.values.get('classroomID')
+    try:
+        insertString = ('SELECT status from Classrooms where classroomID=%(classroomID)s')
+        cursor.execute(insertString,{'classroomID':info['classroomID']})
+        rows = cursor.fetchall()
+        connection.commit()
+        if len(rows) == 0:
+            errors.append("No classroom found!")
+        elif len(rows) > 1:
+            errors.append("Unexpect error:two or more classroom have same ID!")
+        else:
+            info['status'] = rows[0][0]
+            if info['status'] == 1:
+                insertString = ('SELECT courseName from History where classroomID=%(classroomID)s and returnTime IS NULL' )
+                cursor.execute(insertString,{'classroomID':info['classroomID']})
+                rows = cursor.fetchall()
+                connection.commit()
+                if len(rows) == 0:
+                    errors.append("Unexpect error:Classroom has been lent,but history can't find up!")
+                info['courseName'] = rows
+            else:
+                info['courseName'] = "NULL"
+    except Exception:
+        traceback.print_exc()
+        connection.rollback()
+        errors.append('Select fail')
+    info['errors'] = errors
     return jsonify(info)
