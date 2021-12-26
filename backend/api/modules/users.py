2fbb69d86906a50294a011843108e8c82cf451de
adam9876543210 <86069188+adam9876543210@users.noreply.github.com>
2021-12-27 01:33:49 +0800

    Update users.py

--- backend/api/modules/users.py
+++ backend/api/modules/users.py
@@ -2,6 +2,7 @@ from ctypes import sizeof
 from flask import Blueprint,request,jsonify,url_for,redirect,render_template,session
 import datetime
 import pymysql
+from pymysql import cursors
 import yaml
 import re
 import traceback
@@ -415,6 +416,7 @@ def checkLendClassroom():
             info['errors'] = 'checkLendClassroom fail'
     return jsonify(info)
 
+<<<<<<< Updated upstream
 @users.route('/info',methods=['POST'])
 def info():
     connection = pymysql.connect(host=cfg['db']['host'],user=cfg['db']['user'],password=cfg['db']['password'],db=cfg['db']['database'])
@@ -475,6 +477,46 @@ def downGrade():
     cursor.execute("UPDATE Users SET isAdmin =%(isAdmin)s WHERE schoolName=%(schoolName)s AND isAdmin=1",{'isAdmin': 0,'schoolName':info['schoolName']})
     connection.commit()
     return jsonify(info)
+=======
+
+@users.route('/checkReturnClassrrom',method=['POST'])
+def checkReturnClassroom():
+    connection = pymysql.connect(host=cfg['db']['host'],user=cfg['db']['user'],password=cfg['db']['password'],db=cfg['db']['database'])
+    info = dict()
+    cursor = connection.cursor()
+    #ApplicationForms's PK = classroomID department lendTime weekDay
+    info['schoolName'] = request.values.get('schoolName')
+    info['userName'] = request.values.get('userName')
+    info['classroomID'] = request.values.get('classroomID')
+    info['lendTime'] = request.values.get('lendTime')
+    info['weekDay'] = request.values.get('weekDay')
+    try:
+        #update user state first
+        insertString = 'UPDATE Users SET status = 1 WHERE schoolName=(%(schoolName)s);'
+        cursor.execute(insertString, {'schoolName':info['schoolName']})
+        connection.commit() #submit the data to database 
+        #search the reason from ApplicationForms
+        insertString = 'SELECT courseName,userName,reason from ApplicationForms WHERE classroomID=(%(classroomID)s) AND department=(%(department)s) AND lendTime=(%(lendTime)s) AND weekDay=(%(weekDay)s);'
+        cursor.execute(insertString,{'classroomID':info['classroomID'],'department':info['department'],'lendTime':info['lendTime'],'weekDay':info['weekDay']})
+        rows = cursor.fetchall()
+        connection.commit()
+        info['lendTime'] = datetime.date.today()
+        info['courseName'] = rows[0][0]
+        info['userName'] = rows[0][1]
+        info['reason'] = rows[0][2]
+        #print(rows)
+        #insert new data to history
+        insertString = 'INSERT INTO History(classroomID,department,courseName,userName,schoolName,lendTime,returnTime,reason)values(%(classroomID)s,%(department)s,%(courseName)s,%(userName)s,%(schoolName)s,%(lendTime)s,NULL,%(reason)s);'
+        cursor.execute(insertString,{'classroomID':info['classroomID'],'department':info['department'],'courseName':info['courseName'],'userName':info['userName'],'schoolName':info['schoolName'],'lendTime':info['lendTime'],'reason':info['reason']})
+        connection.commit()
+    except Exception: #get exception if there's still occured something wrong
+            traceback.print_exc()
+            connection.rollback()
+            info['errors'] = 'checkLendClassroom fail'
+    return jsonify(info)
+    
+    
+>>>>>>> Stashed changes
 
 #   email confirm undo
 #   if a user input an error email (but legal), his student's ID fucked up. 
